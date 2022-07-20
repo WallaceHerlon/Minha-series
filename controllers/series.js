8b61a99424261ab993863f59ee348e025a4ee5af
Wallace Herlon <wherlon@hotmail.com>
2022-07-19 21:30:00 -0300

    Excluir, editar e organizar a lista de séries

--- controllers/series.js
+++ controllers/series.js
@@ -1,19 +1,50 @@
+const labels = [
+    { id: 'to-watch', name: 'Para Assistido'},
+    { id: 'watching', name: 'Assistindo'},
+    { id: 'watched', name: 'Assistido'}
+]
+
 const index = ({ Serie }, req, res) => {
     Serie.find({}, (err, docs) => {
-        res.render('series/index', { series: docs })
-    })  
+        res.render('series/index', { series: docs, labels })
+    })
 }
+
 const novaProcess = ({ Serie }, req, res) => {
     const serie = new Serie(req.body)
     serie.save(() => {
-       res.redirect('/series') 
-    }) 
+        res.redirect('/series')
+    })
 }
 
 const novaForm = (req, res) => {
     res.render('series/nova')
 }
 
+const excluir = ({ Serie }, req, res) => {
+    Serie.remove({
+        _id: req.params.id
+    }, (err) => {
+        res.redirect('/series')
+    })
+}
+
+const editarProcess = ({ Serie }, req, res) => {
+    Serie.findOne({ _id: req.params.id }, (err, serie) => {
+        serie.name = req.body.name
+        serie.status = req.body.status
+        serie.save()
+        res.redirect('/series')
+    })
+}
+
+const editarForm = ({ Serie }, req, res) => {
+    Serie.findOne({ _id: req.params.id }, (err, serie) => {
+        res.render('series/editar', { serie, labels })
+    })
+
+}
+
 module.exports = {
-    index, novaProcess, novaForm
+    index, novaProcess, novaForm, excluir, editarForm, editarProcess
 }
